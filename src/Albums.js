8009b2c4d9d85e5907157bb15041c73bf7cf728f
AdithyaSanyal <adithyasanyal@gmail.com>
2020-10-28 11:32:05 +0530

    Removed error of repeating search result in albums and tracks page

--- src/Albums.js
+++ src/Albums.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {StyleSheet, Text, View, ScrollView,ActivityIndicator,FlatList} from 'react-native';
 import {Appbar, Searchbar, Card, Title} from 'react-native-paper';
 import Menu from 'react-native-vector-icons/Ionicons';
@@ -8,35 +8,24 @@ import {useNavigation} from '@react-navigation/native';
 
 const Albums = () => {
   const [a,setA]=useState([]);
-  const [album,setAlbum]=useState({});
+  const [album,setAlbum]=useState([]);
+  const [renderList,setRenderList]=useState(false);
   const [text,setText]=useState('');
   const [loading,setLoading]=useState(false);
   const navigation = useNavigation();
+
+  
 
   const fetchData= async ()=>{
     setLoading(true);
     await fetch(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${text}&api_key=00a0a3d918b14a22e2c3d6d079383b84&format=json`,)
     .then((res) => res.json())
     .then((data) => {
-        for(var i=0;i<20;i++){
-          // console.log(data.results['albummatches']['album'][i]['name']);
-          a.push({
-            album:data.results['albummatches']['album'][i]['name'],
-            name:data.results['albummatches']['album'][i]['artist'],
-            key:i,
-          });
-        }
-        console.log(a);
-        setA(a);
-        setLoading(false);
-        setText('');
+      setAlbum(data.results.albummatches.album);
+      setLoading(false);
+      setText('');
       });
   }
-
-  const empty=()=>{
-    setA([]);
-  }
-
 
   return (
     <View style={styles.container}>
@@ -54,20 +43,21 @@ const Albums = () => {
           placeholder="Search for albums"
           style={{marginHorizontal: 30}}
           onChangeText={(text) => setText(text)}
-          onClear={empty}
           onIconPress={fetchData}
         />
       </Appbar.Header>
 
       {loading ? <ActivityIndicator size="large" color="black" /> : null}
       <FlatList
-      data={a}
-      keyExtractor={(element)=>element.key.toString()}
+      data={album}
+      keyExtractor={(element)=>element.url.toString()}
       renderItem={({item})=>{
         return <Text>{item.name}</Text>
-        }}
-      
-      />
+        //Image:item.image[2]['#text']
+        //artist:item.artist
+        //musicURL=item.url
+        
+        }}/>
 
 
     </View>
--- src/Tracks.js
+++ src/Tracks.js
@@ -8,7 +8,7 @@ import {useNavigation} from '@react-navigation/native';
 
 const Tracks = () => {
   const [arr,setA]=useState([]);
-  const [track,setTrack]=useState({});
+  const [track,setTrack]=useState([]);
   const [text,setText]=useState('');
   const [loading,setLoading]=useState(false);
   const navigation = useNavigation();
@@ -18,26 +18,12 @@ const Tracks = () => {
     await fetch(`http://ws.audioscrobbler.com/2.0/?method=track.search&track=${text}&api_key=00a0a3d918b14a22e2c3d6d079383b84&format=json`,)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data.results['trackmatches']['track'][0]['name']);
-        for(var i=0;i<10;i++){
-          console.log(data.results['trackmatches']['track'][1]['artist']);
-          arr.push({
-            track:data.results['trackmatches']['track'][i]['name'],
-            artist:data.results['trackmatches']['track'][i]['artist'],
-            key:i,
-          });
-        }
-        console.log(arr);
-        setA(arr);
+        setTrack(data.results.trackmatches.track);
+        console.log(track);
         setLoading(false);
         setText('');
       });
   }
-
-  const empty=()=>{
-    setA([]);
-  }
-
 
   return (
     <View style={styles.container}>
@@ -55,17 +41,21 @@ const Tracks = () => {
           placeholder="Search for tracks"
           style={{marginHorizontal: 30}}
           onChangeText={(text) => setText(text)}
-          onClear={empty}
           onIconPress={fetchData}
         />
       </Appbar.Header>
 
       {loading ? <ActivityIndicator size="large" color="black" /> : null}
       <FlatList
-      data={arr}
-      keyExtractor={(element)=>element.key.toString()}
+      data={track}
+      keyExtractor={(element)=>element.url.toString()}
       renderItem={({item})=>{
         return <Text>{item.artist}</Text>
+        //name:item.name
+        //musicURL:item.url
+        //listeners:item.listeners
+        //image:item.image[2]['#text']
+        
         }}
       
       />
